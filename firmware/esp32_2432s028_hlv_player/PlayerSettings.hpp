456c030f068989c2c941b9c8d3c62b573f3de0c4
Vitaliy Rudik <rudolff@ya.ru>
2026-07-25 10:14:56 +0200

    Default legacy ESP32 SD clock to 40 MHz

--- firmware/esp32_2432s028_hlv_player/PlayerSettings.hpp
+++ firmware/esp32_2432s028_hlv_player/PlayerSettings.hpp
@@ -8,6 +8,6 @@ constexpr bool kScaleVideoToDisplay = false;
 
 // The CYD2USB microSD socket is connected to the otherwise unused VSPI bus.
 constexpr char kVideoPath[] = "/sdcard/video.hlv";
-constexpr int kSdClockKhz = 20000;
+constexpr int kSdClockKhz = 40000;
 
 }  // namespace player_settings
